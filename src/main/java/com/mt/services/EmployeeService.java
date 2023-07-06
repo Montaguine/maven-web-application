da8293d0aa5aaa4a54ddf07bc7272ea613da5d7e
Yuri Cifuentes <108194047+Montaguine@users.noreply.github.com>
2023-07-06 20:51:37 -0300

    Update EmployeeService.java

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -22,11 +22,11 @@ public class EmployeeService {
 			throws JSONException {
 
 		JSONObject js = new JSONObject();
-		js.put("Name", "Mithun Technologies");
-		js.put("Calling Name", "Mithun");
-		js.put("DOB", "08-Nov-2011");
-		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
-		js.put("Places he like", "His native place");
+		js.put("Name", "Yuri");
+		js.put("Calling Name", "Aluno");
+		js.put("DAT", "06-JUL-2023");
+		js.put("Hobbies", "Trabalhar, dormir");
+		js.put("Places he like", "Casa");
 
 		return js.toString();
 }
